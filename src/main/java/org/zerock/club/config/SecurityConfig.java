ab066752c41258bdaa79d84f4083abd7ae6b3390
keeeeeey <sseioul@naver.com>
2022-10-26 23:30:27 +0900

    [FEAT] 소셜 로그인 기능 추가

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package org.zerock.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.zerock.club.security.handler.ClubLoginSuccessHandler;
+import org.zerock.club.security.service.ClubUserDetailsService;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -35,15 +40,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> {
-            auth.antMatchers("/sample/all").permitAll();
-            auth.antMatchers("/sample/member").hasRole("USER");
-        });
+
+//        http.authorizeHttpRequests((auth) -> {
+//            auth.antMatchers("/sample/all").permitAll();
+//            auth.antMatchers("/sample/member").hasRole("USER");
+//        });
 
         http.formLogin();
         http.csrf().disable();
         http.logout();
 
+        http.oauth2Login().successHandler(successHandler());
+
+        http.rememberMe()
+                .tokenValiditySeconds(60*60*24*7)
+                .userDetailsService(userDetailsService);
+
         return http.build();
+    }
+
+    @Bean
+    public ClubLoginSuccessHandler successHandler() {
+        return new ClubLoginSuccessHandler(passwordEncoder());
     }
 }
